37dd7c8025611ab71ae6a03adce7b386fcc18cf8
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2023-03-12 01:14:12 +0300

    rewrited index page

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,82 @@
-import React, { useState } from 'react';
-import Router from 'next/router';
-import Octicon, { MarkGithub } from '@primer/octicons-react';
-import { Head } from '../components';
-import styled from 'styled-components';
-import { theme, mixins } from '../style';
-const { colors, fonts } = theme;
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Head, UserInfo, Charts, Repos, Footer, Corner, Error, RateLimit } from '../components';
+import GhPolyglot from 'gh-polyglot';
 
-const StyledContainer = styled.div`
-  ${mixins.flexCenter};
-  background-color: ${colors.black};
-  background-image: linear-gradient(${colors.black} 0%, ${colors.darkGrey} 100%);
-  color: ${colors.offWhite};
-  height: 100vh;
+const GITHUB_TOKEN = process.env.GITHUB_PERSONAL_TOKEN;
 
-  form {
-    background-color: transparent;
-    border-radius: 5px;
-    padding: 2rem;
-    margin-bottom: 20vh;
-    max-width: 600px;
-    text-align: center;
-    svg {
-      color: ${colors.blue};
-    }
-    label {
-      display: block;
-      font-size: 2.5rem;
-      font-weight: 500;
-      margin: 2rem;
-    }
-    input {
-      background-color: #26303c;
-      outline: 0;
-      border: 0;
-      border-radius: 0.25rem;
-      width: 100%;
-      max-width: 500px;
-      margin: 0 auto;
-      padding: 1rem;
-      color: ${colors.lightblue};
-      font-family: ${fonts.mono};
-      font-size: 2rem;
-      font-weight: 400;
-      text-align: center;
-    }
+const FETCH_HEADERS = {
+  Authorization: `Token ${GITHUB_TOKEN}`,
+}
 
-    .submit {
-      ${mixins.blueButton};
-      margin-top: 3rem;
-      filter: none;
+const fetchUserProfileData = async () => {
+  const response = await fetch(`https://api.github.com/user`, {headers: FETCH_HEADERS})
+  return await response.json();
+}
+
+const fetchUserReposData = async (repos_count) => {
+  const REPOS_PER_PAGE = 30;
+  const numbersOfPages = Math.ceil(repos_count / REPOS_PER_PAGE);
+  let repos = []
+
+  for (let pageNumber = 1; pageNumber <= numbersOfPages; pageNumber++) {
+    const response = await fetch(
+      `https://api.github.com/user/repos?page=${pageNumber}&per_page=${REPOS_PER_PAGE}`,
+      {headers: FETCH_HEADERS}
+    );
+    const fetched_repos = await response.json();
+    repos = repos.concat(fetched_repos);
+  }
+
+  return repos;
+}
+
+export const getStaticProps = async () => {
+  const userData = await fetchUserProfileData();
+  const reposData = await fetchUserReposData(userData.public_repos);
+
+  return {
+    props: {
+      userData: userData,
+      repoData: reposData,
+      langData: [],
     }
   }
-`;
+}
 
-const Home = () => {
-  const [username, setUsername] = useState('');
-  const handleChange = e => setUsername(e.target.value);
+const Home = props => {
+  const {userData, repoData, langData} = props;
+
+  const getLangData = () => {
+    const me = new GhPolyglot(`${username}`);
+    me.userStats((err, stats) => {
+      if (err) {
+        console.error('Error:', err);
+        setError({ active: true, type: 400 });
+      }
+      setLangData(stats);
+    });
+  };
 
   return (
     <main>
-      <Head title="OctoProfile" />
+      <Head title={`OctoProfile | ${userData.login}`} />
 
-      <StyledContainer>
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            Router.push({
-              pathname: '/user',
-              query: { id: username },
-            });
-          }}>
-          <Octicon icon={MarkGithub} size="large" />
-          <label htmlFor="username">Find Your OctoProfile</label>
-          <input name="username" type="text" onChange={handleChange} />
-        </form>
-      </StyledContainer>
+      <Corner />
+
+      {userData && <UserInfo userData={userData} />}
+
+      {langData && repoData && <Charts langData={langData} repoData={repoData} />}
+
+      {repoData && <Repos repoData={repoData} />}
+
+      <Footer />
     </main>
   );
+};
+
+Home.propTypes = {
+  query: PropTypes.object,
 };
 
 export default Home;
